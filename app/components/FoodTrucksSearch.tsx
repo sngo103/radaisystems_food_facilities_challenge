2f290cfcc05e567e46db9d6f834650e21859f83c
Samantha Ngo <sngo.134@gmail.com>
2025-08-12 14:15:07 -0400

    Added address search

--- app/components/FoodTrucksSearch.tsx
+++ app/components/FoodTrucksSearch.tsx
@@ -6,33 +6,43 @@ type FoodTrucksSearchProps = {
 }
 
 export default function FoodTrucksSearch({ handleSearch }: FoodTrucksSearchProps) {
-  const [query, setQuery] = useState("");
-  const [statusFilter, setStatusFilter] = useState<PermitStatus>(PermitStatus.ALL);
+  const [addressQuery, setAddressQuery] = useState("");
+  const [applicantQuery, setApplicantQuery] = useState("");
+  const [applicantStatusFilter, setApplicantStatusFilter] = useState<PermitStatus>(PermitStatus.ALL);
+  const [addressStatusFilter, setAddressStatusFilter] = useState<PermitStatus>(PermitStatus.ALL);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
+    setApplicantQuery(e.target.value);
   };
 
-  const handleSubmit = (e: any) => {
+  const handleSubmitApplicant = (e: any) => {
     e.preventDefault();
-    if(query.trim() !== '') {
-      handleSearch(query, ["applicant"], statusFilter);
+    if(applicantQuery.trim() !== '') {
+      handleSearch(applicantQuery, ["applicant"], applicantStatusFilter);
+    }
+  };
+
+  const handleSubmitAddress = (e: any) => {
+    e.preventDefault();
+    if(addressQuery.trim() !== '') {
+      handleSearch(addressQuery, ["address"], addressStatusFilter);
     }
   };
 
   return (
+    <div>
     <div className="container px-4 mt-8 mx-auto w-full cursor-auto">
-      <text className="font-sans text-l m-1">Applicant Search</text>
+      <text className="font-sans text-l m-1">Search by Applicant Name</text>
       <input
         type="text"
-        value={query}
+        value={applicantQuery}
         onChange={handleChange}
-        placeholder="Search for applicant..."
+        placeholder="Search by applicant name..."
         className="border border-gray-300 p-2"
       />
       <text className="font-sans text-l m-1"></text>with status
-      <select className="font-sans mx-1 py-1 border-0.5 rounded-sm" name="status" id="status" value={statusFilter} 
-      onChange={(e) => setStatusFilter(e.target.value as PermitStatus)}>
+      <select className="font-sans mx-1 py-1 border-0.5 rounded-sm" name="status" id="status" value={applicantStatusFilter} 
+      onChange={(e) => setApplicantStatusFilter(e.target.value as PermitStatus)}>
         <option className="font-sans m-1 border-none" value={PermitStatus.APPROVED}>{PermitStatus.APPROVED}</option>
         <option value={PermitStatus.EXPIRED}>{PermitStatus.EXPIRED}</option>
         <option value={PermitStatus.REQUESTED}>{PermitStatus.REQUESTED}</option>
@@ -40,9 +50,33 @@ export default function FoodTrucksSearch({ handleSearch }: FoodTrucksSearchProps
         <option value={PermitStatus.ISSUED}>{PermitStatus.ISSUED}</option>
         <option value={PermitStatus.ALL} selected={true}>{PermitStatus.ALL}</option>
       </select>
-      <button className="mt-2 rounded-sm bg-yellow-400 text-white m-1 hover:bg-yellow-500 cursor-pointer px-8 py-1" onClick={handleSubmit}>
-        Search
+      <button className="mt-2 rounded-sm bg-yellow-400 text-white m-1 hover:bg-yellow-500 cursor-pointer px-8 py-1" onClick={handleSubmitApplicant}>
+        Search By Applicant Name
       </button>
+      </div>
+      <div className="container px-4 mt-2 mx-auto w-full cursor-auto">
+      <text className="font-sans text-l m-1">Search by Address</text>
+      <input
+        type="text"
+        value={addressQuery}
+        onChange={(e) => setAddressQuery(e.target.value)}
+        placeholder="Search by address..."
+        className="border border-gray-300 p-2"
+      />
+      <text className="font-sans text-l m-1"></text>with status
+      <select className="font-sans mx-1 py-1 border-0.5 rounded-sm" name="status" id="status" value={addressStatusFilter} 
+      onChange={(e) => setAddressStatusFilter(e.target.value as PermitStatus)}>
+        <option className="font-sans m-1 border-none" value={PermitStatus.APPROVED}>{PermitStatus.APPROVED}</option>
+        <option value={PermitStatus.EXPIRED}>{PermitStatus.EXPIRED}</option>
+        <option value={PermitStatus.REQUESTED}>{PermitStatus.REQUESTED}</option>
+        <option value={PermitStatus.SUSPEND}>{PermitStatus.SUSPEND}</option>
+        <option value={PermitStatus.ISSUED}>{PermitStatus.ISSUED}</option>
+        <option value={PermitStatus.ALL} selected={true}>{PermitStatus.ALL}</option>
+      </select>
+      <button className="mt-2 rounded-sm bg-yellow-400 text-white m-1 hover:bg-yellow-500 cursor-pointer px-8 py-1" onClick={handleSubmitAddress}>
+        Search By Address
+      </button>
+    </div>
     </div>
   );
 }
